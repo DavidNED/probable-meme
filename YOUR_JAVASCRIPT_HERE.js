875718eeb9166fff8dae9000418b39ebcaf8664f
David <d_middelveld@hotmail.com>
2018-10-20 16:36:48 +0200

    next changes

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -9,26 +9,32 @@ weapon: {
     type: 'Gun',
     damage: 2
 }
-}
+}//hero
+
+
 
 const rest = (object) => {
 object.health = 10;
+return object}//rest
 
-return object
 
+const equipWeapon = (hero) => {
+if (hero.inventory.length === 0) {
+return;}
+
+hero.weapon = hero.inventory[0];
+
+
+
+}//equipWeapon
+
+
+
+const pickUpItem = (firstObj, object) => {
+firstObj.inventory[1] = object;
+
+const newObj = {type: 'dagger', 'damage': 2}
+firstObj.inventory.push(newObj);
 }
 
-const equipWeapon = () => {
-
-
-}
-
-
-const pickUpItem = (first, second) => {
-const put = first.second
-first.inventory[1] = second
-
-
-
-}
 
