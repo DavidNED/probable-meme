3e4ad8a0d45cea7e4373390d447fd079815be020
David <d_middelveld@hotmail.com>
2018-10-20 10:51:45 +0200

    my first update

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,3 +1,34 @@
 // Write your JS here
 
-const 
+const hero = {
+name: 'David',
+heroic: false,
+inventory: [],
+health: 10,
+weapon: {
+    type: 'Gun',
+    damage: 2
+}
+}
+
+const rest = (object) => {
+object.health = 10;
+
+return object
+
+}
+
+const equipWeapon = () => {
+
+
+}
+
+
+const pickUpItem = (first, second) => {
+const put = first.second
+first.inventory[1] = second
+
+
+
+}
+
